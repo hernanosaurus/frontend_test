2a90b4b286d7b903e0ceef582a5ff767e63bf800
hernanosaurus <hterania.dev@gmail.com>
2023-11-09 04:32:49 +0800

    [FEATURE] Api Fetching

--- app/types/user.ts
+++ app/types/user.ts
@@ -5,25 +5,10 @@ export type User = {
   email: string;
   address: Address;
   phone: string;
-  website: string;
-  company: Company;
 };
 
 export type Address = {
   street: string;
   suite: string;
   city: string;
-  zipcode: string;
-  geo: Geo;
-};
-
-export type Company = {
-  name: string;
-  catchPhrase: string;
-  bs: string;
-};
-
-export type Geo = {
-  lat: string;
-  lng: string;
 };
